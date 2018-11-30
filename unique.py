4a1722f71f2b7ab5ded4806adfdc7cc8ca61beb3
Tarun047 <gtarun047@gmail.com>
2018-11-30 14:39:24 +0530

    modified:   unique.py

--- unique.py
+++ unique.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from collections import Counter
+def main():
+    n = int(input())
+    a = Counter([int(x) for x in input().split()])
+    #print(a)
+    ans = []
+    for x in a:
+        if a[x]>1:
+            ans.append(str(x))
+    if not ans:
+        print("unique")
+    else:
+        print(' '.join(ans))
+
+
+try:
+    main()
+except:
+    print("Invalid Input")
